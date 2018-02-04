38b76b1b370e3ace533043697b05b516a4ffe829
Loan Nguyen <nguyenloan150492@gmail.com>
2018-02-04 12:39:59 +0200

    initial

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { PagesMainComponent } from './pages-main/pages-main';
+@NgModule({
+	declarations: [PagesMainComponent],
+	imports: [],
+	exports: [PagesMainComponent]
+})
+export class ComponentsModule {}

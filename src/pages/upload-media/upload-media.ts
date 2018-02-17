762db732f1b7bfbe452a97588c7c46e0d74a4b3b
perseus0832 <nickvo2308@gmail.com>
2018-02-17 16:34:36 +0200

    update

--- src/pages/upload-media/upload-media.ts
+++ src/pages/upload-media/upload-media.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Provider } from '@angular/core/src/di/provider';
+import { MediaProvider } from '../../providers/media/media';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
 
 /**
  * Generated class for the UploadMediaPage page.
@@ -14,9 +17,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'upload-media.html',
 })
 export class UploadMediaPage {
+  file: File;  
+  //media = new Media(this.file, '', '');
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private mediaProvider: MediaProvider) { }
+
+  setFile(evt){
+    console.log(evt.target.files[0]);
+    this.file = evt.target.files[0];
+    
   }
+
+  startUpload(){
+    //Create FormData object
+    //add title & discription, add file,send FormData obj
+
+    const form: FormData = new FormData();
+    form.append('file',this.file);
+    // form.append('title',this.media.title);
+    // form.append('description',this.media.description);
+    // console.log(form);
+    // console.log(this.media);
+    
+      
+    this.mediaProvider.uploadFile(form).subscribe(data => {
+      console.log(data);
+    },(e:HttpErrorResponse) => {
+      console.log(e);
+    });
+    }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad UploadMediaPage');
--- src/providers/media/media.ts
+++ src/providers/media/media.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
 
 /*
   Generated class for the MediaProvider provider.
@@ -10,8 +11,25 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MediaProvider {
 
+  apiUrl = 'http://media.mw.metropolia.fi/wbma';
+  apiUrl_user = 'http://media.mw.metropolia.fi/wbma/users/user';
+  apiUrl_media = 'http://media.mw.metropolia.fi/wbma/media';
+
   constructor(public http: HttpClient) {
     console.log('Hello MediaProvider Provider');
+  }
+
+  public uploadFile(media){
+    const settings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token')),
+    };
+    console.log(settings);
+    console.log(this.http.post(this.apiUrl_media, media, settings));
+    return this.http.post(this.apiUrl_media, media, settings);
+  }
+
+  public getAllMedia(){
+    return this.http.get(this.apiUrl + '/media');
   }
 
 }

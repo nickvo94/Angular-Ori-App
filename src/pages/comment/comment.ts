901b9c37cf62fd7caf6dac73fbe039a5587ca024
Loan Nguyen <nguyenloan150492@gmail.com>
2018-02-28 12:35:52 +0200

    UI detailpage - add commentpage

---
+++ src/pages/comment/comment.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the CommentPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@Component({
+  selector: 'page-comment',
+  templateUrl: 'comment.html',
+})
+export class CommentPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CommentPage');
+  }
+
+}
